16583dad8d0d763d733b85a4997666bc5f3133bc
Tywunda Shivers <tywunda.shivers@gmail.com>
2019-10-05 22:48:41 -0700

    adding modified index.html styles.css script.js

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 // Generate random user password
-function generate(){
-     
-    //Password length
-    length = document.getElementById("pwlength").value;
 
-    //Random password values
-    var alphaNumericaCharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+//Password length
+var length = document.getElementById("length").value;
 
-    password = "";
+//Random password values
+var alphaNumericaCharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-    //Create a for loop to choose random password characters
-    for(var i =0; i <= length; i++){
-        password = password + alphaNumericaCharacters.charAt(Math.floor(Math.random() * Math.floor(alphaNumericaCharacters.length - 1)));
+password = "";
+
+//Create a for loop to choose random password characters
+for (var i =0; i <= length; i++) {
+    password = password + alphaNumericaCharacters.charAt(Math.floor(Math.random() * Math.floor(alphaNumericaCharacters.length - 1)));
     }
 
-    //Add password to textarea
-    document.getElementById("password").alphaNumericaCharacters = password;
+//Add password to textarea
+document.getElementById("password").alphaNumericaCharacters = password;
 
-    //Set default length display to 8
-    document.getElementById("pwlength").innerHTML = "Length: 8";
+//Set default length display to 8
+document.getElementById("length").innerHTML = "length: 8";
 
-    //Function added to set length based on pwlength position
-    document.getElementById("pwlength").oninput = function (){
+//Function added to set length based on length position
+document.getElementById("length").oninput = function (){
 
-        if(document.getElementById("pwlength").value > 0){
-            document.getElementById("length").innerHTML = "Length" + document.getElementById("pwlength").value;
-        }
-        else {
-            document.getElementById("length").innerHTML = "Length: 1";
-        }
+if(document.getElementById("length").alphaNumericaCharacters > 0){
+    document.getElementById("length").innerHTML = "length" + document.getElementById("length").alphaNumericaCharacters;
+    }
 
-    //Function added to copy password to clipboard
-    function copyPassword(){
+else {
+     document.getElementById("length").innerHTML = "length: 1";
+    }
+
+//Function added to copy password to clipboard
+function copyPassword(){
         
-        document.getElementById("display").select();
+    document.getElementById("display").select();
 
-        document.execCommand("Copy");
-        alert("Password Copied To Clipboard");
-    }
-       
-    }
-}
+    document.execCommand("Copy");
+
+    alert("Password Copied To Clipboard");
